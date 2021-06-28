e7b02e878efb12762a2e328edfb94ba847cc09c2
Alex Kim <alexkim251@yahoo.com>
2021-06-28 13:37:29 -0500

    added basic outline boilerplate code for Setup page

---
+++ src/components/ProgressionSetup/ProgressionSetup.jsx
@@ -0,0 +1,12 @@
+import React, {useState} from 'react';
+import {useSelector} from 'react-redux';
+
+function Setup() {
+    return (
+        <div>
+
+        </div>
+    );
+}
+
+export default Setup;
