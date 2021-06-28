6d6db00500c53d3f59362e5e7510002699169406
Alex Kim <alexkim251@yahoo.com>
2021-06-28 15:26:59 -0500

    added PostSetupSaga to axios.post in sagas folder

---
+++ src/redux/sagas/setup.saga.js
@@ -0,0 +1,18 @@
+import { put, takeEvery } from 'redux-saga/effects';
+import axios from 'axios';
+
+function* PostSetup(action) {
+    try{
+        //send payload from setup page to api/progression
+        yield axios.post('/api/progression', action.payload);
+        yield put({ type: 'SET_PROGRESSION_DETAILS' });
+    } catch(error){
+        console.log('Error saga axios post request:', error);
+    }
+}
+
+function* PostSetupSaga() {
+    yield takeEvery('ADD_PROGRESSION', PostSetup);
+  }
+
+  export default PostSetupSaga;
