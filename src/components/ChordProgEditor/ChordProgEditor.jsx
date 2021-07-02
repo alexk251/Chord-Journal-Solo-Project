80cf06580ca64cb6e7cacfe48fb3d2381c433f38
Alex Kim <alexkim251@yahoo.com>
2021-07-02 17:34:40 -0500

    added metronomic chord playback

--- src/components/ChordProgEditor/ChordProgEditor.jsx
+++ src/components/ChordProgEditor/ChordProgEditor.jsx
@@ -6,8 +6,23 @@ import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
 import './ChordProgEditor.css';
 
+import Timer from './timer.js';
+import { Howler, Howl } from 'howler';
+import { transpose, note, Chord, ChordType } from '@tonaljs/tonal';
+import soundFile2 from './pianosprite.mp3';
+import click from './click2.mp3'
 
 function ChordProgEditor() {
+
+    let count = 0;
+    let current_chord = 0;
+    let isRunning = false;
+       // if i choose to show intervals
+       let [chordIntervalsSpelled, setChordIntervalsSpelled] = useState({
+        spelling: ''
+    })
+    // if i choose to show notes in chord
+    let [chordNotesSpelled, setChordNotesSpelled] = useState('');
 
     const dispatch = useDispatch();
     const history = useHistory();
@@ -33,11 +48,94 @@ function ChordProgEditor() {
         history.push('/user');
     }
 
+    const handlePlayProgression = () => {
+        count = 0;
+        current_chord = 0;
+    if (!isRunning) {
+        isRunning = true;
+        metronome.start();
+    } else {
+        isRunning = false;
+        metronome.stop();
+        
+    }
+    }
+
+    const metronome = new Timer(playChords, 60000 / (progression.tempo), { immediate: false });
+
+
+    function playChords() {
+        console.log(count);
+        console.log(progression.beat_per_measure)
+        console.log(chords[current_chord])
+         if (current_chord == (progression.amount_of_chords)-1) {
+            current_chord = 0;
+        }
+        if (count == progression.beat_per_measure) {
+            count = 0;
+            PlayChords(chords[current_chord]);
+            current_chord++;
+        };
+        
+        count++;
+        
+
+    }
+
+    // logic that plays chords
+       //Chord Player Logic starts here
+
+       const sound = new Howl({
+        src: [soundFile2],
+        onload() {
+            console.log('Sound File has been loaded.');
+            soundEngine2.init();
+        },
+        onloaderror(e, msg) {
+            console.log('Error', e, msg)
+        }
+    })
+
+    function PlayChords(usedChord) {
+        let chordIntervals = Chord.getChord(usedChord.chord_quality, usedChord.root_note).intervals;
+        console.log(chordIntervals);
+        const startNoteWithOctave = usedChord.root_note + usedChord.octave;
+        console.log(startNoteWithOctave);
+        let chordNotes = chordIntervals.map(val => {
+            return transpose(startNoteWithOctave, val);
+        })
+        console.log(chordNotes)
+        soundEngine2.play(chordNotes);
+    }
+
+    const soundEngine2 = {
+        // logic that takes mp3 and divides it into intervals
+        init() {
+            const lengthOfNote = 2400;
+            let timeIndex = 0;
+            for (let i = 24; i <= 96; i++) {
+                sound['_sprite'][i] = [timeIndex, lengthOfNote];
+                timeIndex += lengthOfNote;
+            }
+        },
+        // logic that turns notes into midi values and plays them
+        play(soundSequence) {
+            const chordMidiNUmbers = soundSequence.map(noteName => {
+                return note(noteName).midi;
+            });
+            Howler.volume(0.20);
+            chordMidiNUmbers.forEach(noteMidiNumber => {
+                sound.play(noteMidiNumber.toString());
+            })
+        }
+    }
+
+
     return (
         <div>
             <h1 className='text-center'>Chord-Progression Editor</h1>
             <div className='text-center'>
-            <Button variant='contained' color='default'>Play Chords</Button>
+            <Button onClick={handlePlayProgression} variant='contained' color='default'>Play Chords</Button>
             <Button onClick={handleHome} variant='contained' color='default'>Return to Home</Button>
             </div>
             <br/>
