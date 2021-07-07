129a5ed9ae50d83c8b18ac6b2338d9c1208dc9f9
Alex Kim <alexkim251@yahoo.com>
2021-07-07 09:38:51 -0500

    added comments to  userItem component

--- src/components/UserItem/UserItem.jsx
+++ src/components/UserItem/UserItem.jsx
@@ -4,14 +4,17 @@ import Button from '@material-ui/core/Button';
 
 
 function UserItem({progression}) {
-
+    // history for going to different pages/routes
     const history = useHistory();
-
+    // dispatch for sending payloads and calling sagas and reducers
     const dispatch = useDispatch();
 
     const selectProgression = (event) => {
+        // calls set progression details with progression when progression is selected
         dispatch({type:'SET_PROGRESSION_DETAILS', payload: progression})
+        // fetches chords for progression when progression is selected
         dispatch({ type: 'FETCH_CHORDS', payload: progression.id });
+        // goes to details page
         history.push('/details')
     }
 
